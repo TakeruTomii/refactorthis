b0797fa741b7b6420a2a7eb24fee4758c9269e19
TakeruTomii <dev.haunted.house@gmail.com>
2024-05-12 17:03:31 +1200

    Remove redundant else clause

--- RefactorThis.Domain/InvoiceService.cs
+++ RefactorThis.Domain/InvoiceService.cs
@@ -23,125 +23,123 @@ namespace RefactorThis.Domain
 			{
 				throw new InvalidOperationException(ErrorMessages.ERROR_NO_INVOICE_MATCHING_THIS_PAYMENT);
 			}
-			else
-			{
-				if ( invoice.Amount == 0 )
-				{
-					if ( invoice.Payments == null || !invoice.Payments.Any( ) )
-					{
-						responseMessage = ResponseMessages.RES_NO_PAYMENT_NEEDED;
-					}
-					else
-					{
-						throw new InvalidOperationException(ErrorMessages.ERROR_INVOICE_INVALID_STATE);
-					}
-				}
-				else
-				{
-					if ( invoice.Payments != null && invoice.Payments.Any( ) )
-					{
-						if ( invoice.Payments.Sum( x => x.Amount ) != 0 && invoice.Amount == invoice.Payments.Sum( x => x.Amount ) )
-						{
-							responseMessage = ResponseMessages.RES_INVOICE_ALREADY_FULLY_PAID;
-						}
-						else if ( invoice.Payments.Sum( x => x.Amount ) != 0 && payment.Amount > ( invoice.Amount - invoice.AmountPaid ) )
-						{
-							responseMessage = ResponseMessages.RES_PAYMENT_GREATER_THAN_PARTIAL_AMOUNT_REMAINING;
-						}
-						else
-						{
-							if ( ( invoice.Amount - invoice.AmountPaid ) == payment.Amount )
-							{
-								switch ( invoice.Type )
-								{
-									case InvoiceType.Standard:
-										invoice.AmountPaid += payment.Amount;
-										invoice.Payments.Add( payment );
-										responseMessage = ResponseMessages.RES_FINAL_INVOICE_FULLY_PAID;
-										break;
-									case InvoiceType.Commercial:
-										invoice.AmountPaid += payment.Amount;
-										invoice.TaxAmount += payment.Amount * InvoiceConstants.TAX_RATE;
-										invoice.Payments.Add( payment );
-										responseMessage = ResponseMessages.RES_FINAL_INVOICE_FULLY_PAID;
-										break;
-									default:
-										throw new ArgumentOutOfRangeException( );
-								}
-								
-							}
-							else
-							{
-								switch ( invoice.Type )
-								{
-									case InvoiceType.Standard:
-										invoice.AmountPaid += payment.Amount;
-										invoice.Payments.Add( payment );
-										responseMessage = ResponseMessages.RES_ANOTHER_PARTIAL_NOT_FULLY_PAID;
-										break;
-									case InvoiceType.Commercial:
-										invoice.AmountPaid += payment.Amount;
-										invoice.TaxAmount += payment.Amount * InvoiceConstants.TAX_RATE;
-										invoice.Payments.Add( payment );
-										responseMessage = ResponseMessages.RES_ANOTHER_PARTIAL_NOT_FULLY_PAID;
-										break;
-									default:
-										throw new ArgumentOutOfRangeException( );
-								}
-							}
-						}
-					}
-					else
-					{
-						if ( payment.Amount > invoice.Amount )
-						{
-							responseMessage = ResponseMessages.RES_PAYMENT_GREATER_THAN_INVOICE_AMOUNT;
-						}
-						else if ( invoice.Amount == payment.Amount )
-						{
-							switch ( invoice.Type )
-							{
-								case InvoiceType.Standard:
-									invoice.AmountPaid = payment.Amount;
-									invoice.TaxAmount = payment.Amount * InvoiceConstants.TAX_RATE;
-									invoice.Payments.Add( payment );
-									responseMessage = ResponseMessages.RES_INVOICE_NOW_FULLY_PAID;
-									break;
-								case InvoiceType.Commercial:
-									invoice.AmountPaid = payment.Amount;
-									invoice.TaxAmount = payment.Amount * InvoiceConstants.TAX_RATE;
-									invoice.Payments.Add( payment );
-									responseMessage = ResponseMessages.RES_INVOICE_NOW_FULLY_PAID;
-									break;
-								default:
-									throw new ArgumentOutOfRangeException( );
-							}
-						}
-						else
-						{
-							switch ( invoice.Type )
-							{
-								case InvoiceType.Standard:
-									invoice.AmountPaid = payment.Amount;
-									invoice.TaxAmount = payment.Amount * InvoiceConstants.TAX_RATE;
-									invoice.Payments.Add( payment );
-									responseMessage = ResponseMessages.RES_INVOICE_NOW_PARTIALLY_PAID;
-									break;
-								case InvoiceType.Commercial:
-									invoice.AmountPaid = payment.Amount;
-									invoice.TaxAmount = payment.Amount * InvoiceConstants.TAX_RATE;
-									invoice.Payments.Add( payment );
-									responseMessage = ResponseMessages.RES_INVOICE_NOW_PARTIALLY_PAID;
-									break;
-								default:
-									throw new ArgumentOutOfRangeException( );
-							}
-						}
-					}
-				}
-			}
-			
-			invoice.Save();
+
+            if (invoice.Amount == 0)
+            {
+                if (invoice.Payments == null || !invoice.Payments.Any())
+                {
+                    responseMessage = ResponseMessages.RES_NO_PAYMENT_NEEDED;
+                }
+                else
+                {
+                    throw new InvalidOperationException(ErrorMessages.ERROR_INVOICE_INVALID_STATE);
+                }
+            }
+            else
+            {
+                if (invoice.Payments != null && invoice.Payments.Any())
+                {
+                    if (invoice.Payments.Sum(x => x.Amount) != 0 && invoice.Amount == invoice.Payments.Sum(x => x.Amount))
+                    {
+                        responseMessage = ResponseMessages.RES_INVOICE_ALREADY_FULLY_PAID;
+                    }
+                    else if (invoice.Payments.Sum(x => x.Amount) != 0 && payment.Amount > (invoice.Amount - invoice.AmountPaid))
+                    {
+                        responseMessage = ResponseMessages.RES_PAYMENT_GREATER_THAN_PARTIAL_AMOUNT_REMAINING;
+                    }
+                    else
+                    {
+                        if ((invoice.Amount - invoice.AmountPaid) == payment.Amount)
+                        {
+                            switch (invoice.Type)
+                            {
+                                case InvoiceType.Standard:
+                                    invoice.AmountPaid += payment.Amount;
+                                    invoice.Payments.Add(payment);
+                                    responseMessage = ResponseMessages.RES_FINAL_INVOICE_FULLY_PAID;
+                                    break;
+                                case InvoiceType.Commercial:
+                                    invoice.AmountPaid += payment.Amount;
+                                    invoice.TaxAmount += payment.Amount * InvoiceConstants.TAX_RATE;
+                                    invoice.Payments.Add(payment);
+                                    responseMessage = ResponseMessages.RES_FINAL_INVOICE_FULLY_PAID;
+                                    break;
+                                default:
+                                    throw new ArgumentOutOfRangeException();
+                            }
+
+                        }
+                        else
+                        {
+                            switch (invoice.Type)
+                            {
+                                case InvoiceType.Standard:
+                                    invoice.AmountPaid += payment.Amount;
+                                    invoice.Payments.Add(payment);
+                                    responseMessage = ResponseMessages.RES_ANOTHER_PARTIAL_NOT_FULLY_PAID;
+                                    break;
+                                case InvoiceType.Commercial:
+                                    invoice.AmountPaid += payment.Amount;
+                                    invoice.TaxAmount += payment.Amount * InvoiceConstants.TAX_RATE;
+                                    invoice.Payments.Add(payment);
+                                    responseMessage = ResponseMessages.RES_ANOTHER_PARTIAL_NOT_FULLY_PAID;
+                                    break;
+                                default:
+                                    throw new ArgumentOutOfRangeException();
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if (payment.Amount > invoice.Amount)
+                    {
+                        responseMessage = ResponseMessages.RES_PAYMENT_GREATER_THAN_INVOICE_AMOUNT;
+                    }
+                    else if (invoice.Amount == payment.Amount)
+                    {
+                        switch (invoice.Type)
+                        {
+                            case InvoiceType.Standard:
+                                invoice.AmountPaid = payment.Amount;
+                                invoice.TaxAmount = payment.Amount * InvoiceConstants.TAX_RATE;
+                                invoice.Payments.Add(payment);
+                                responseMessage = ResponseMessages.RES_INVOICE_NOW_FULLY_PAID;
+                                break;
+                            case InvoiceType.Commercial:
+                                invoice.AmountPaid = payment.Amount;
+                                invoice.TaxAmount = payment.Amount * InvoiceConstants.TAX_RATE;
+                                invoice.Payments.Add(payment);
+                                responseMessage = ResponseMessages.RES_INVOICE_NOW_FULLY_PAID;
+                                break;
+                            default:
+                                throw new ArgumentOutOfRangeException();
+                        }
+                    }
+                    else
+                    {
+                        switch (invoice.Type)
+                        {
+                            case InvoiceType.Standard:
+                                invoice.AmountPaid = payment.Amount;
+                                invoice.TaxAmount = payment.Amount * InvoiceConstants.TAX_RATE;
+                                invoice.Payments.Add(payment);
+                                responseMessage = ResponseMessages.RES_INVOICE_NOW_PARTIALLY_PAID;
+                                break;
+                            case InvoiceType.Commercial:
+                                invoice.AmountPaid = payment.Amount;
+                                invoice.TaxAmount = payment.Amount * InvoiceConstants.TAX_RATE;
+                                invoice.Payments.Add(payment);
+                                responseMessage = ResponseMessages.RES_INVOICE_NOW_PARTIALLY_PAID;
+                                break;
+                            default:
+                                throw new ArgumentOutOfRangeException();
+                        }
+                    }
+                }
+            }
+
+            invoice.Save();
 
 			return responseMessage;
 		}
