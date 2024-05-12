5c877c2e83df9d8f5c06ee83b3a8e24fc016eba3
TakeruTomii <dev.haunted.house@gmail.com>
2024-05-12 18:15:27 +1200

    refactor method

--- RefactorThis.Domain/InvoiceService.cs
+++ RefactorThis.Domain/InvoiceService.cs
@@ -15,71 +15,109 @@ namespace RefactorThis.Domain
 
 		public string ProcessPayment( Payment payment )
         {
-            var invoice = _invoiceRepository.GetInvoice(payment.Reference);
-
             var responseMessage = string.Empty;
+
+            IInvoice invoice = GetValidatedInvoice(payment);
+
+            responseMessage = ProcessPayment(payment, invoice);
+
+            invoice.Save();
+
+            return responseMessage;
+        }
+
+        private IInvoice GetValidatedInvoice(Payment payment)
+        {
+            var invoice = _invoiceRepository.GetInvoice(payment.Reference);
 
             if (invoice == null)
             {
                 throw new InvalidOperationException(ErrorMessages.ERROR_NO_INVOICE_MATCHING_THIS_PAYMENT);
             }
 
+            return invoice;
+        }
+
+        private static string ProcessPayment(Payment payment, IInvoice invoice)
+        {
+            string responseMessage;
             if (IsPaymentNeeded(invoice))
             {
-                if (HasPaymentInInvoice(invoice))
-                {
-                    throw new InvalidOperationException(ErrorMessages.ERROR_INVOICE_INVALID_STATE);
-                }
-                responseMessage = ResponseMessages.RES_NO_PAYMENT_NEEDED;
+                responseMessage = ProcessNoPaymentInvoice(invoice);
             }
             else
             {
                 if (HasPaymentInInvoice(invoice))
                 {
-                    if (IsInvoiceAlreadyPaid(invoice))
-                    {
-                        responseMessage = ResponseMessages.RES_INVOICE_ALREADY_FULLY_PAID;
-                    }
-                    else if (IsOverPaidForRemainingInvoice(payment, invoice))
-                    {
-                        responseMessage = ResponseMessages.RES_PAYMENT_GREATER_THAN_PARTIAL_AMOUNT_REMAINING;
-                    }
-                    else
-                    {
-                        if (IsFullyPaidForRemainingInvoice(payment, invoice))
-                        {
-                            responseMessage = ResponseMessages.RES_FINAL_INVOICE_FULLY_PAID;
-                        }
-                        else
-                        {
-                            responseMessage = ResponseMessages.RES_ANOTHER_PARTIAL_NOT_FULLY_PAID;
-                        }
-                        invoice.AddPayments(payment);
-                    }
+                    responseMessage = ProcessExistingInvoice(payment, invoice);
                 }
                 else
                 {
-                    if (IsOverPaid(payment, invoice))
-                    {
-                        responseMessage = ResponseMessages.RES_PAYMENT_GREATER_THAN_INVOICE_AMOUNT;
-                    }
-                    else
-                    {
-                        if(IsFullyPaid(payment, invoice))
-                        {
-                            responseMessage = ResponseMessages.RES_INVOICE_NOW_FULLY_PAID;
-                        }
-                        else
-                        {
-                            responseMessage = ResponseMessages.RES_INVOICE_NOW_PARTIALLY_PAID;
-                        }
-                        invoice.AddPayments(payment);
-                        
-                    }
+                    responseMessage = ProcessNewInvoice(payment, invoice);
                 }
             }
 
-            invoice.Save();
+            return responseMessage;
+        }
+
+        private static string ProcessNoPaymentInvoice(IInvoice invoice)
+        {
+            string responseMessage;
+            if (HasPaymentInInvoice(invoice))
+            {
+                throw new InvalidOperationException(ErrorMessages.ERROR_INVOICE_INVALID_STATE);
+            }
+            responseMessage = ResponseMessages.RES_NO_PAYMENT_NEEDED;
+            return responseMessage;
+        }
+
+        private static string ProcessExistingInvoice(Payment payment, IInvoice invoice)
+        {
+            string responseMessage;
+            if (IsInvoiceAlreadyPaid(invoice))
+            {
+                responseMessage = ResponseMessages.RES_INVOICE_ALREADY_FULLY_PAID;
+            }
+            else if (IsOverPaidForRemainingInvoice(payment, invoice))
+            {
+                responseMessage = ResponseMessages.RES_PAYMENT_GREATER_THAN_PARTIAL_AMOUNT_REMAINING;
+            }
+            else
+            {
+                if (IsFullyPaidForRemainingInvoice(payment, invoice))
+                {
+                    responseMessage = ResponseMessages.RES_FINAL_INVOICE_FULLY_PAID;
+                }
+                else
+                {
+                    responseMessage = ResponseMessages.RES_ANOTHER_PARTIAL_NOT_FULLY_PAID;
+                }
+                invoice.AddPayments(payment);
+            }
+
+            return responseMessage;
+        }
+
+        private static string ProcessNewInvoice(Payment payment, IInvoice invoice)
+        {
+            string responseMessage;
+            if (IsOverPaid(payment, invoice))
+            {
+                responseMessage = ResponseMessages.RES_PAYMENT_GREATER_THAN_INVOICE_AMOUNT;
+            }
+            else
+            {
+                if (IsFullyPaid(payment, invoice))
+                {
+                    responseMessage = ResponseMessages.RES_INVOICE_NOW_FULLY_PAID;
+                }
+                else
+                {
+                    responseMessage = ResponseMessages.RES_INVOICE_NOW_PARTIALLY_PAID;
+                }
+                invoice.AddPayments(payment);
+
+            }
 
             return responseMessage;
         }
